e109986648160f13b54b062125171002ae5b1d4b
tomer <tomerg11e@gmail.com>
2021-04-29 12:53:53 +0300

    main dummy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import FeatureExtraction as fE
+
 def main():
-    print("aaaa it is working, celebrate")
-    print("am i an cont?")
+    file_path = r"Data/train1.wtag"
+    feature_statistics = fE.FeatureStatistics(file_path)
+    feature_id = fE.FeatureID(feature_statistics, 20)
+    test_dict = feature_id.words_tags_dict()
+    print(test_dict)
+
 
 
 if __name__ == '__main__':
