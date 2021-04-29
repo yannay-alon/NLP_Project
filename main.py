2d23221373a64060e99bd797756e75296ad36fcb
yannay-alon <62138811+yannay-alon@users.noreply.github.com>
2021-04-29 13:01:28 +0300

    Extracting features (basics)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import FeatureExtraction as fE
+from FeatureExtraction import FeatureStatistics, FeatureID
+
 
 def main():
     file_path = r"Data/train1.wtag"
-    feature_statistics = fE.FeatureStatistics(file_path)
-    feature_id = fE.FeatureID(feature_statistics, 20)
+    feature_statistics = FeatureStatistics(file_path)
+    feature_id = FeatureID(feature_statistics, 20)
     test_dict = feature_id.words_tags_dict()
     print(test_dict)
-
 
 
 if __name__ == '__main__':
