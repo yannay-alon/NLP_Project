602610b482a359ba5627407303de2030a6f52ca4
yannay-alon <62138811+yannay-alon@users.noreply.github.com>
2021-05-06 14:23:50 +0300

    Started History

--- FeatureExtraction/History.py
+++ FeatureExtraction/History.py
@@ -1,20 +1,52 @@
 from .FeatureID import FeatureID
-from typing import List
+from .TextEditor import TextEditor
+from typing import List, Tuple, Iterable
+import random
+import math
 
 
 class History:
 
-    def __init__(self, feature_id: "FeatureID", history_length: int):
+    def __init__(self, feature_id: "FeatureID", text_editor: "TextEditor"):
         self.feature_id = feature_id
-        self.history_length = history_length
+        self.text_editor = text_editor
+        self.history_length = text_editor.window_size
 
     def history_to_vector(self, history_words: List[str], history_tags: List[str]):
         features = []
         pass
 
-    def create_histories(self, file_path, max_number: int, style: str, **kwargs) -> List["History"]:
+    def create_histories(self, max_number: int = None, style: str = None, **kwargs) -> \
+            Iterable[Tuple[Iterable[str], Iterable[str]]]:
 
-        with open(file_path) as f:
-            for line in f:
-                pass
+        def increment(start, step, end):
+            yield_counter = 0
+            line_index = -1
+            for line, decorated_line in self.text_editor.read_file(cyclic=True):
+                line_index += 1
 
+                if line_index < start:
+                    continue
+                if (line_index - start) % step != 0:
+                    continue
+
+                split_words = decorated_line.split(" ")
+
+                k_grams = zip(*[split_words[i:] for i in range(self.history_length)])
+                for k_gram in k_grams:
+                    split_list = (word_tag.split("_") for word_tag in k_gram)
+                    words, tags = list(zip(*split_list))
+                    yield words, tags
+
+                    yield_counter += 1
+                    if end is not None and end <= yield_counter:
+                        raise StopIteration
+
+        if style == "ALL":
+            return increment(0, 1, max_number)
+        elif style == "RANDOM":
+            return increment(random.randint(0, self.text_editor.text_size // 2),
+                             random.randint(1, int(math.sqrt(self.text_editor.text_size))),
+                             max_number)
+        elif style == "INCREMENT":
+            return increment(kwargs["start"], kwargs["step"], max_number)
--- FeatureExtraction/TextEditor.py
+++ FeatureExtraction/TextEditor.py
@@ -1,22 +1,29 @@
+from typing import Tuple
+
+
 class TextEditor:
 
     def __init__(self, file_path: str, window_size: int):
         self.file_path = file_path
         self.window_size = window_size
+        self.text_size = sum(1 for line in open(file_path))
 
-    def read_file(self):
-        with open(self.file_path) as file:
-            for line in file:
-                # Remove \n at the end of the line
-                line = line[:-1]
+    def read_file(self, cyclic: bool = False) -> Tuple[str, str]:
+        while True:
+            with open(self.file_path) as file:
+                for line in file:
+                    # Remove \n at the end of the line
+                    line = line[:-1]
 
-                # Special symbols for the beginning and ending of the line
-                start = "(╯°□°）╯︵┻━┻".replace(" ", "").replace("_", "")
-                start = f"{start}_{start} " * (self.window_size - 1)
+                    # Special symbols for the beginning and ending of the line
+                    start = "(╯°□°）╯︵┻━┻".replace(" ", "").replace("_", "")
+                    start = f"{start}_{start} " * (self.window_size - 1)
 
-                end = "┬─┬ノ(゜-゜ノ)".replace(" ", "").replace("_", "")
-                end = f"{end}_{end}"
+                    end = "┬─┬ノ(゜-゜ノ)".replace(" ", "").replace("_", "")
+                    end = f"{end}_{end}"
 
-                decorated_line = f"{start}{line} {end}"
+                    decorated_line = f"{start}{line} {end}"
 
-                yield line, decorated_line
+                    yield line, decorated_line
+            if not cyclic:
+                break
